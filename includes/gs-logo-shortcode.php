b73abc191e32553d1a90ac54f9db56526908fba7
samdani <samdani@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-30 06:25:55 +0000

    initial
    git-svn-id: https://plugins.svn.wordpress.org/gs-logo-slider/trunk@1123386 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/gs-logo-shortcode.php
@@ -0,0 +1,91 @@
+<?php 
+
+add_action('wp_footer','gs_ls_slider_trigger');
+
+function gs_ls_slider_trigger(){
+?>
+<script type="text/javascript">
+jQuery(document).ready(function(){
+  jQuery('.gs_logo_container').bxSlider({
+  	slideWidth: 200,
+    minSlides: 1,
+    maxSlides: 5,
+    slideMargin: 10,
+  	moveSlides: 1,
+  	speed: 750,
+  	controls: true,
+  	autoHover: true,
+  	pager: false,
+  	auto: true
+  });
+ 
+});
+</script>
+<?php
+}
+
+
+// ---------- Shortcode [gs_logo] -------------
+
+add_shortcode( 'gs_logo', 'gs_logo_shortcode' );
+
+function gs_logo_shortcode( $atts ) {
+
+	extract(shortcode_atts( 
+			array(
+			'posts' 	=> -1,
+			'order_by'	=> 'title',
+			'orderby' 	=> 'title',
+			'title'		=> 'yes'
+			), $atts 
+		));
+
+	$loop = new WP_Query(
+		array(
+			'post_type'	=> 'gs-logo-slider',
+			'order_by'	=> 'title',
+			'title'		=> 'yes'
+			)
+		);
+
+	$output = '<div class="gs_logo_container">';
+		if ( $loop->have_posts() ) {
+			
+			while ( $loop->have_posts() ) {
+				$loop->the_post();
+				$meta = get_post_meta( get_the_id() );
+				
+				$gs_logo_id = get_post_thumbnail_id();
+				$gs_logo_url = wp_get_attachment_image_src($gs_logo_id, array(200,125), true);
+				$gs_logo = $gs_logo_url[0];
+				$gs_logo_alt = get_post_meta($gs_logo_id,'_wp_attachment_image_alt',true);
+
+				$output .= '<div class="gs_logo_single">';
+
+					if ($meta['client_url'][0]) :
+				 		$output .= '<a href="'. $meta['client_url'][0] .'" target="_blank">';
+				 	endif;
+
+				 	if ($gs_logo) :
+						$output .= '<img src="'.$gs_logo.'" alt="'.$gs_logo_alt.'" >';
+					endif;
+
+					if ($meta['client_url'][0]) :
+						$output .= '</a>';
+					endif;
+					
+					if ( $title == "yes" ) :
+						$output .= '<h3 class="gs_logo_title">'. get_the_title() .'</h3>';
+					endif;
+				$output .= '</div>';
+			}
+
+		} else {
+			$output .= "No Logo Added!";
+		}
+
+	$output .= '</div>';
+
+	return $output;
+}
+
---
+++ readme.txt
@@ -0,0 +1,69 @@
+=== GS Logo Slider ===
+Contributors: samdani
+Donate link: http://www.gsamdani.me/contact
+Tags: Best logo slider, client logo slider, easy logo slider, logo slider, logo slider wordpress, owl carousel, owl carousel wordpress, owl carousel wordpress plugin, Responsive logo slider, gsamdani
+Requires at least: 3.3
+Tested up to: 4.1
+Stable tag: 1.0.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Best Responsive Logo Slider to display partners, clients or sponsor's Logo.
+
+== Description ==
+
+GS Logo Slider is a WordPress plugin which create custom post type to add client's Logo to display anywhere of your site using shortcode. 
+
+GS Logo Slider plugin is simple but flexible & powerful.
+
+*   [View live Demos &raquo;](http://logo.gsamdani.me)
+*   [Support &raquo;](http://www.gsamdani.me/support)
+*   [Contact Me &raquo;](http://www.gsamdani.me/contact)
+
+Plugin Features
+
+* Logo Shortcode Ready
+* Responsive Logo slider.
+* Very light weight.
+* Custom post type for Logo posting.
+* No need any settings.
+* Work with all Wordpress theme.
+* Easy to use.
+* Developer friendly & easy to customize.
+* Powered by OWL bxSlider.
+* Its easy to use interface allows you to manage, edit, create, and delete Testimonials with no new knowledge
+
+
+Shortcode
+
+`[gs_logo]`
+
+== Installation ==
+
+* Install it as a regular WordPress plugin
+
+* Active the plugin.
+
+* Use this [gs_logo] shortcode, where you need to show the logo slider.
+
+
+== Frequently Asked Questions ==
+
+* Coming Soon
+
+== Screenshots ==
+
+* Coming Soon
+
+
+== Changelog ==
+
+= version 1.0.2 =
+
+= version 1.0 =
+* Initial release
+
+== Upgrade notice ==
+
+= version 1.0 =
+* Initial release
